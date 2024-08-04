f1fe85184ff763922a34aef4656501c9abc34d4e
IGC2k <158501327+IGC2k@users.noreply.github.com>
2024-08-03 21:33:04 -0700

    Updated library_insertions.sql

--- backend/library_insertions.sql
+++ backend/library_insertions.sql
@@ -1,7 +1,7 @@
 use library;
 
 INSERT INTO 'librarybranch' VALUES (1, 'Dr. Martin Luther King, Jr. Libary', '150 E San Fernando St., San Jose, CA 95112');
-INSERT INTO 'librarybranch' VALUES (2, 'Cupertion Library', '10800 Torre Ave, Cupertion, Cupertion, CA 95014');
+INSERT INTO 'librarybranch' VALUES (2, 'Cupertino Library', '10800 Torre Ave, Cupertino, Cupertino, CA 95014');
 INSERT INTO 'librarybranch' VALUES (3, 'Sunnyvale Public Library', '665 W Olive Ave, Sunnyvale, CA 94086');
 INSERT INTO 'librarybranch' VALUES (4, 'Mountain View Public Library', '585 Franklin St, Mountain View, CA 94041');
 INSERT INTO 'librarybranch' VALUES (5, 'Milpitas Library', '160 N Main St, Milpitas, CA 95035');
@@ -12,5 +12,6 @@ insert into person(uname,usertype,preferredbranch) values('Amy','U','3');
 insert into person(uname,usertype,preferredbranch) values('Bob','U','4');
 insert into person(uname,usertype,preferredbranch) values('Alice','U','5');
 
-
 insert into item(title,author,edition,libraryBranchID,ItemType,standardNumber,copies) values();
+
+insert into rating(ratingdate,itemid,personid,stars) values();
