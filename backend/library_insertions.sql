cc9acc5d6ad7c1964c5faded31c831a8e76a82de
Samuel <samuel@Samuels-MacBook-Pro.local>
2024-08-03 20:43:55 -0700

    added libraries

--- backend/library_insertions.sql
+++ backend/library_insertions.sql
@@ -1,6 +1,10 @@
 use library;
 
-INSERT INTO 'librarybranch' VALUES (1, 'Dr. Martin Luther King, Jr. Libary', '150 E San Fernando St. at San Jose, CA95112');
+INSERT INTO 'librarybranch' VALUES (1, 'Dr. Martin Luther King, Jr. Libary', '150 E San Fernando St., San Jose, CA 95112');
+INSERT INTO 'librarybranch' VALUES (2, 'Cupertion Library', '10800 Torre Ave, Cupertion, Cupertion, CA 95014');
+INSERT INTO 'librarybranch' VALUES (3, 'Sunnyvale Public Library', '665 W Olive Ave, Sunnyvale, CA 94086');
+INSERT INTO 'librarybranch' VALUES (4, 'Mountain View Public Library', '585 Franklin St, Mountain View, CA 94041');
+INSERT INTO 'librarybranch' VALUES (5, 'Milpitas Library', '160 N Main St, Milpitas, CA 95035');
 
 insert into person(uname,usertype,preferredbranch) values('Ivan','U','1');
 
