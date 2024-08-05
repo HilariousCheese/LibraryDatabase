2797693d55de305d072cd5f64ebeab0288edf222
IGC2k <158501327+IGC2k@users.noreply.github.com>
2024-08-05 02:42:19 -0700

    Updated StoreProcedure.sql

--- backend/StoreProcedure.sql
+++ backend/StoreProcedure.sql
@@ -1,6 +1,6 @@
 use library;
 /*Create a stored procedure that returns the total number of copies of a book our library owns, given a title 
-A. First,find the standard number(S) of A. 
+A. First, find the standard number(S) of A. 
 Then, go through the ITEM table to find tuples where standard Number= S. 
 Then, add the number of copies together to find the total number of copies the whole library system owns.*/
 
@@ -9,10 +9,10 @@ DROP PROCEDURE IF EXISTS FindNumberOfCopies;
 DELIMITER //
 CREATE PROCEDURE FindNumberOfCopies ( IN itemTitle varchar(150),  IN itemEdition varchar(150), IN itemAuthor varchar(150), OUT result int(11))
 BEGIN
- select sum(copies) into result from item where title = itemTitle and edition=itemEdition and author=itemAuthor;
+ select sum(copies) into result from item where title=itemTitle and edition=itemEdition and author=itemAuthor;
 END //
 DELIMITER ;
 
-call FindNumberOfCopies( 'Venezuela, a democracy', '1st ed.','Allen, Henry Justin, 1868-1950.', @result);
+call FindNumberOfCopies('Database Management Systems','3rd ed.','Ramakrishnan, Gehrke', @result);
 select @result;
 
